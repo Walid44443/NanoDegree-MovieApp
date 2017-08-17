370469077a53962b9704305a0a8846aec7bfd617
walid4444 <midoma4444@gmail.com>
2017-08-17 16:22:28 +0200

    Initial commit

---
+++ src/main/java/com/mal/walid/moviewalid/Functions.java
@@ -0,0 +1,8 @@
+package com.mal.walid.moviewalid;
+
+/**
+ * Created by walid4444 on 7/26/17.
+ */
+
+public class Functions {
+}
---
+++ src/main/java/com/mal/walid/moviewalid/SqlLite/MovieProvider.java
@@ -0,0 +1,8 @@
+package com.mal.walid.moviewalid.SqlLite;
+
+/**
+ * Created by walid4444 on 7/25/17.
+ */
+
+public class MovieProvider {
+}
